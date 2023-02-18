091d27aad9d9e34a4b838a656b0bd89596dec50f
Oleh Kim <olehkim@example.com>
2023-02-18 20:35:36 +0900

    front

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,7 +1,6 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { Redirect } = require('@nestjs/common');
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
 /**
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,67 +1,62 @@
-import { useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { UserDTO } from "@my-shop/dtos";
 
 export function Index() {
-  const email = useRef<HTMLInputElement>(null);
-  const password = useRef<HTMLInputElement>(null);
-
-  function signInClick (event) {
-      event.preventDefault();
-
-     fetch(process.env.baseUrl + "/signin", {
-         method: "POST",
-          headers: {
-             "Accept": 'application/json',
-             'Content-Type': 'application/json',   
-           },
-         body: JSON.stringify({
-             email: email.current.value,
-             password: password.current.value,
-         })  
-     }).then((res)=>{
-      if(res.status === 201){
-        return res.json();
-      }else{
-          alert("wrong email or password")
-      }
-     })
-     .then((data)=>{
-      if(data.Authorization){
-          // document.cookie = "auth=" + data.Authorization
-          // window.location.href="/main";
-     }
-  })
-     .catch(()=>{alert("something wrong")});
-  }
+    const {register, handleSubmit} = useForm<UserDTO>();
+    const signInClick: SubmitHandler<UserDTO> = (data) => {
+        fetch(process.env.baseUrl + "/signin", {
+            method: "POST",
+            headers: {
+                "Accept": 'application/json',
+                'Content-Type': 'application/json',   
+            },
+            body: JSON.stringify({
+                email: data.email,
+                password: data.password,
+            }),  
+        }).then((res)=>{
+            if(res.status === 201){
+                return res.json();
+            }else{
+                alert("wrong email or password")
+            }
+        }).then((data)=>{
+            if(data.Authorization){
+                document.cookie = "auth=" + data.Authorization
+                window.location.href="/main";
+            }
+        }).catch(()=>{alert("something wrong, try again")});
+    }
 
 
-  return (
-    <div className="container">
-    <div className="screen">
-        <div className="screen__content">
-            <form className="login">
-                <div className="login__field">
-                    <i className="login__icon fas fa-user"></i>
-                    <input type="text" ref={email} className="login__input" placeholder="User name / Email"/>
+    return (
+        <div className="container">
+            <div className="screen">
+                <div className="screen__content">
+                    <form className="login" onSubmit={handleSubmit(signInClick)}>
+                        <div className="login__field">
+                            <i className="login__icon fas fa-user"></i>
+                            <input type="text" {...register("email")} className="login__input" placeholder="User name / Email"/>
+                        </div>
+                        <div className="login__field">
+                            <i className="login__icon fas fa-lock"></i>
+                            <input type="password" {...register("password", {required: true, min:8})} className="login__input" placeholder="Password"/>
+                        </div>
+                        <button className="button login__submit">
+                            <span className="button__text">Log In Now</span>
+                            <i className="button__icon fas fa-chevron-right"></i>
+                        </button>				
+                    </form>
                 </div>
-                <div className="login__field">
-                    <i className="login__icon fas fa-lock"></i>
-                    <input type="password" ref={password} className="login__input" placeholder="Password"/>
-                </div>
-                <button onClick={signInClick} className="button login__submit">
-                    <span className="button__text">Log In Now</span>
-                    <i className="button__icon fas fa-chevron-right"></i>
-                </button>				
-            </form>
-        </div>
-        <div className="screen__background">
-            <span className="screen__background__shape screen__background__shape4"></span>
-            <span className="screen__background__shape screen__background__shape3"></span>		
-            <span className="screen__background__shape screen__background__shape2"></span>
-            <span className="screen__background__shape screen__background__shape1"></span>
-        </div>		
+                <div className="screen__background">
+                    <span className="screen__background__shape screen__background__shape4"></span>
+                    <span className="screen__background__shape screen__background__shape3"></span>		
+                    <span className="screen__background__shape screen__background__shape2"></span>
+                    <span className="screen__background__shape screen__background__shape1"></span>
+                </div>		
+            </div>
     </div>
-</div>
-  );
+    );
 }
 
 export default Index;
